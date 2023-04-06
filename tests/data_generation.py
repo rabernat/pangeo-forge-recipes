1956e20b107af4294962ad76f425f44ea79a868a
Julius Busecke <julius@ldeo.columbia.edu>
2023-04-06 16:06:48 -0400

    remove more cftime stuff

--- tests/data_generation.py
+++ tests/data_generation.py
@@ -3,16 +3,13 @@ import pandas as pd
 import xarray as xr
 
 
-def make_ds(nt=10, non_dim_coords=False, add_extra_dim_coords=False, use_cftime=False):
+def make_ds(nt=10, non_dim_coords=False, add_extra_dim_coords=False):
     """Return a synthetic random xarray dataset."""
     np.random.seed(2)
     # TODO: change nt to 11 in order to catch the edge case where
     # items_per_input does not evenly divide the length of the sequence dimension
     ny, nx, ne = 18, 36, 2
-    if use_cftime:
-        time = xr.cftime_range(start="2010-01-01", periods=nt, freq="D")
-    else:
-        time = pd.date_range(start="2010-01-01", periods=nt, freq="D")
+    time = pd.date_range(start="2010-01-01", periods=nt, freq="D")
     lon = (np.arange(nx) + 0.5) * 360 / nx
     lon_attrs = {"units": "degrees_east", "long_name": "longitude"}
     lat = (np.arange(ny) + 0.5) * 180 / ny
@@ -45,7 +42,6 @@ def make_ds(nt=10, non_dim_coords=False, add_extra_dim_coords=False, use_cftime=
 
     # Add time coord encoding
     # Remove "%H:%M:%s" as it will be dropped when time is 0:0:0
-    # if not use_cftime:
     ds.time.encoding = {
         "units": f"days since {time[0].strftime('%Y-%m-%d')}",
         "calendar": "proleptic_gregorian",
